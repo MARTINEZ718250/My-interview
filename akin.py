84ad656f0a393372fb360a807e5cd36ecf0a35a5
MARTINEZ718250 <akinmartins994@gmail.com>
2024-08-27 05:17:52 -0700

    Add files via upload

---
+++ akin.py
@@ -0,0 +1,12 @@
+# A simple countdown program using a while loop
+counter = 10
+
+while counter > 0:
+    print("Current count:", counter)
+    counter -= 1  # Decrease the counter by 1 each time
+
+print("Done with the countdown!")
+#Explanation:
+#Setting Up the Counter: I started with a variable counter set to 5.
+#Running the Loop: The while loop keeps running as long as counter is more than 0. Each time the loop runs, the counter goes down by 1.
+#Finishing the Countdown: When the loop finishes, I print a final message to indicate the countdown is complete.
---
+++ my first phyton work.py
@@ -0,0 +1,14 @@
+# A simple program to work with a list of fruits
+fruits = ["apple", "banana", "cherry", "date"]
+print("Here’s my list of fruits:", fruits)
+fruits.append("elderberry")
+print("I added a new fruit to the list:", fruits)
+fruits.remove("banana")
+print("After removing one fruit, the list is now:", fruits)
+print("The first fruit in my list is:", fruits[0])
+print("The last fruit in my list is:", fruits[-1])
+#List Creation: I started by creating a list called fruits with a few items in it.
+#Displaying the List: I printed the original list to see what it contains.
+#Adding an Item: I used append to add a new fruit to the list.
+#Removing an Item: I removed one fruit using remove.
+#Accessing Items: Finally, I accessed and printed the first and last items in the list.
